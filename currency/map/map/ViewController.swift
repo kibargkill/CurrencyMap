8f705258dc0f17530f4d1407fc96f3b0c9d038be
Vlad <kibargkill@icloud.com>
2017-11-23 16:47:30 +0200

    vlad

---
+++ currency/map/map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  map
+//
+//  Created by mymac on 23.11.2017.
+//  Copyright © 2017 mymac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
